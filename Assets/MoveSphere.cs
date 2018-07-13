ffd6941a618ea61601c5acf8e5402a4a9e21d2e8
Samuel Plum <psyborg@taloria.com>
2018-07-12 18:45:26 -0700

    Adding missing Assets Folder

---
+++ Assets/MoveSphere.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSphere : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);	
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit it.");
+        //other.transform.parent.rotation = transform.rotation - other.transform.rotation;
+        transform.rotation = other.transform.rotation;
+    }
+}
